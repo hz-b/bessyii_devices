346c59ccb4f410b6188b4107819656435c8b0ca2
Will Smith <william.smith@helmholtz-berlin.de>
2021-12-06 10:56:42 +0100

    add new undulator definition

--- bessyii_devices/undulator.py
+++ bessyii_devices/undulator.py
@@ -29,6 +29,39 @@ class Undulator(Device):
     harmonic_01_nM  = Cpt(EpicsSignalRO,  'BasePmWLength'                   ,kind = 'hinted' )   
     status          = Cpt(EpicsSignalRO,  'BaseStatISLbl' ,string='True'    ,kind = 'config' )   # Stop = done  (could use this for a pv-positioner, generally we let the mono take control though)
 
+# new implementation with PVPositioner
+class Undulator(PVPositioner):
+
+    """
+    Object to query undulator status
+    """
+    
+    setpoint        = Cpt(EpicsSignal,    'BaseParGapsel.B',kind = 'hinted')
+    readback        = Cpt(EpicsSignalRO,  'BaseIPmGap.A',kind = 'hinted', labels={"motors", "undulators"})
+    done            = Cpt(EpicsSignalRO,  'BaseStatISLbl' ,string='True'    ,kind = 'config' )
+    actuate         = Cpt(EpicsSignal,    'BaseCmdCalc.PROC'                                 )
+
+    # Setpoint and Config
+    gap_velocity    = Cpt(EpicsSignal,    'DiagPhyVelSet'                   ,kind = 'config' )
+    gap_delta       = Cpt(EpicsSignal,    'BaseParGapTrs'                   ,kind = 'config' )
+    return_pos      = Cpt(EpicsSignal,    'BaseHomeRPos.A'                  ,kind = 'config' )
+    
+    # Commands
+    id_control      = Cpt(EpicsSignal,    'BaseCmdLswitch'                  ,kind = 'config' )   # allows us to select control from the panel or from the monochromator panel
+    cmd_stop        = Cpt(EpicsSignal,    'BaseCmnUsrStop',string='True'    ,kind = 'config' )   # an bo record that lets us stop or start (1=stopped, 0=enabled)
+    cmd_sel         = Cpt(EpicsSignal,    'BaseCmdMcmd'   ,string='True'    ,kind = 'config' )   # an mbbo record that lets us select what we are going to do
+    
+    # Readback
+    harmonic_01_eV  = Cpt(EpicsSignalRO,  'BasePmEnergy'                    ,kind = 'hinted' )   # approximated energy of the 1stharmonic with standard electron beam condition
+    harmonic_01_nM  = Cpt(EpicsSignalRO,  'BasePmWLength'                   ,kind = 'hinted' )   
+
+
+
+
+
+
+
+
 class UndulatorU49_2(Device):
 
     """
