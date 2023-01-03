f41c27cca9bc8e67a43ff3fe1d16fa4c226f090a
Smith, William <william.smith@helmholtz-berlin.de>
2023-01-03 15:08:30 +0000

    Created emil_dcm.py and made first attempt at record definition

---
+++ bessyii_devices/emil_dcm.py
@@ -0,0 +1,18 @@
+from ophyd import Device, Component as Cpt, EpicsMotor, EpicsSignalRO
+
+class U17_DCM(Device):
+
+    """
+    Work In Progress
+    
+    An Ophyd Device which exposes control to the EMIL U17 DCM using the EPICS Motor Record Interface
+
+    Instantiated with:
+
+    u17_dcm = U17_DCM("myDCM:")
+    """
+
+    en = Cpt(EpicsMotor, "axis_Energy")
+    theta = Cpt(EpicsMotor, "axis_Theta")
+    theta_cryo = Cpt(EpicsMotor, "pmaxAxis4")
+
